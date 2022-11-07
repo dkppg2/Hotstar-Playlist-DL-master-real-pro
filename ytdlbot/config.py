f293132f61d933062266829213f3b2de6834759f
kevinnadar <jesikamaraj@gmail.com>
2022-11-07 22:42:27 +0530

    init

--- ytdlbot/config.py
+++ ytdlbot/config.py
@@ -57,7 +57,7 @@ MYSQL_USER = os.getenv("MYSQL_USER", "root")
 MYSQL_PASS = os.getenv("MYSQL_PASS", "root")
 
 AUDIO_FORMAT = os.getenv("AUDIO_FORMAT")
-ARCHIVE_ID = os.getenv("ARCHIVE_ID")
+ARCHIVE_ID = int(os.getenv("ARCHIVE_ID", "0"))
 
 IPv6 = os.getenv("IPv6", False)
 ENABLE_FFMPEG = os.getenv("ENABLE_FFMPEG", False)
